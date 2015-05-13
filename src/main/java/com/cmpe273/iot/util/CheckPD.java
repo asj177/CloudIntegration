658a569a9ee4fb8a5c2494326bc25af17c82c85e
Kandarp <kandarp.9@gmail.com>
2015-05-13 16:32:47 -0700

    Check flash drive files changes
    Check flash drive files changes

--- src/main/java/com/cmpe273/iot/util/CheckPD.java
+++ src/main/java/com/cmpe273/iot/util/CheckPD.java
@@ -1,11 +1,40 @@
 package com.cmpe273.iot.util;
 
+import java.io.File;
 
 /**
- * 
- * @author mangesh
+ * Get the list of files from the pd and checks the pd
+ * and return true if the pd is attached
+ * @author Mangesh
  *
  */
 public class CheckPD {
+	
+	
+	public boolean checkPD(){
+		
+		boolean isUSB=false;
+		File file = new File("/media/arpit");
+		String[] directories =file.list();
+		
+		
+		
+		if(directories.length>0){
+			isUSB=true;
+		}
+		
+		return isUSB;
+	}
+	
+	public String getPath(){
+		File file = new File("/media/arpit");
+		String[] directories =file.list();
+		
+		String path="/media/arpit/"+directories[0];
+		return path;
+		
+	}
+	
+	
 
 }
