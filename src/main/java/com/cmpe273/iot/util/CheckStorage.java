3edbf11b65cb745882ddd75a3f129271f6ac066f
Arjun Mantri <arjunmantri@users.noreply.github.com>
2015-05-13 18:32:28 -0700

    Update CheckStorage.java
    Check storage

--- src/main/java/com/cmpe273/iot/util/CheckStorage.java
+++ src/main/java/com/cmpe273/iot/util/CheckStorage.java
@@ -7,5 +7,26 @@ package com.cmpe273.iot.util;
  *
  */
 public class CheckStorage {
+  public long getDropboxSize() throws DbxException {
+        long dbxSizeRemaining = 0;
+        long sizeOfpendrive = 32; // value to be fetched from the code by Mangesh.
+        DbxAccountInfo dbxAccountInfo = dbxClient.getAccountInfo(); //The dbxClient is mentioned in the Demo.java file
+        // in GB :)
+        long shared = dbxAccountInfo.quota.shared / 1024 / 1024 / 1024;
+        long normal = dbxAccountInfo.quota.normal / 1024 / 1024 / 1024;
+        long total  = dbxAccountInfo.quota.total / 1024 / 1024 / 1024;
+        dbxSizeRemaining = total - normal - shared;
+
+        return dbxSizeRemaining;
+
+        if(dbxSizeRemaining < sizeOfpendrive ) return 0;
+
+        else return 1;
+    }
+
+    public Date getTimeStampInfo(){
+        Date dateOfFileFromDrive = new Date(); //need to get the time of file from Mangesh
+        
+    }
 
 }
