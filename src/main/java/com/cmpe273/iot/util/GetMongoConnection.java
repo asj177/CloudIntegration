6bcdf78a3f4167441401c83fddfad18921bd367d
Deepak Kole <deepakrkole@gmail.com>
2015-05-13 18:05:44 -0700

    update Mongo connection

--- src/main/java/com/cmpe273/iot/util/GetMongoConnection.java
+++ src/main/java/com/cmpe273/iot/util/GetMongoConnection.java
@@ -1,5 +1,13 @@
 package com.cmpe273.iot.util;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 /**
  * 
@@ -7,5 +15,29 @@ package com.cmpe273.iot.util;
  *
  */
 public class GetMongoConnection {
+
+	private static MongoClient connection;
+	private static DB dbConnection;
+
+	private GetMongoConnection() {
+
+	}
+
+	public static DB getConnection() throws IOException {
+
+		if (dbConnection == null) {
+			
+			MongoClientURI uri = new MongoClientURI(
+					"mongodb://admin:admin@ds061360.mongolab.com:61360/mongo");
+			connection = new MongoClient(uri);
+			dbConnection = connection.getDB("mongo");
+		}
+		return dbConnection;
+
+	}
+
+	public static void closeConnection() {
+
+	}
 
 }
